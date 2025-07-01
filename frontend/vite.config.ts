1bd6e3afc10e43dcbc09bda89f7172a0a6eb6dd0
Geoffroy Theo <130750391+Kabre57@users.noreply.github.com>
2025-07-01 22:13:46 +0200

    Serveur

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    host: true,
+    host: '0.0.0.0',
     // Configuration proxy pour éviter les problèmes CORS en développement
     proxy: {
       '/api': {
